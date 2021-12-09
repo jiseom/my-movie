a6e44fba66d312969f70d1412a0dbfb11aaafc06
jiseon <paper138@naver.com>
2021-12-09 20:12:32 +0900

    회원가입 폼 데이터 제출 - 유효성 검사

--- src/main/java/com/project/mymovie/account/AccountController.java
+++ src/main/java/com/project/mymovie/account/AccountController.java
@@ -1,12 +1,25 @@
 package com.project.mymovie.account;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
     @GetMapping("/sign-up")
     public String signUpForm() {
         return "account/sign-up-form";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up-form";
+        }
+        //TODO: 회원가입 과정 -> 이메일 인증 메일 발송
+        return "redirect:/";
     }
 }
